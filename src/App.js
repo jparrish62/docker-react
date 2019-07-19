9221d5af73e691109421f34457ed6234052839b1
Julian Parrish <parrishjulian62@gmail.com>
2019-07-18 20:23:32 -0400

    Initial push

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let try this shit again
         </a>
       </header>
     </div>
